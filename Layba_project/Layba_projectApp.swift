a382122535e312b74d5aadce0a9b7ab51f0155a4
Junio Layba <laybajunioeran@gmail.com>
2026-09-10 18:28:11 +0800

    Initial Commit

---
+++ Layba_project/Layba_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layba_projectApp.swift
+//  Layba_project
+//
+//  Created by Mac-LAB on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Layba_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
